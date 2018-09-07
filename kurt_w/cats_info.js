9f2681b48bae18781c594686e9b10258496c731d
kurtw29 <ckurt37@gmail.com>
2018-09-07 10:52:29 -0700

    kurt_w ejs cars and cats

---
+++ kurt_w/cats_info.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'cat1': [
+        {food: "steak", age: 1, sleep:'laptop'},
+        {food: "milk", age: 2, sleep:'couch'},
+        {food: "tuna", age: 3, sleep:'bed'}
+    ],
+    'cat2': [
+        {food: "milk", age: 1, sleep:'peopel lap'},
+        {food: "fish ball", age: 2, sleep:'heater'},
+        {food: "hamburger", age: 3, sleep:'bed'}
+    ],
+    'cat3': [
+        {food: "chow mein", age: 1, sleep:'pillow'},
+        {food: "lasagna", age: 2, sleep:'bed'},
+        {food: "sushi", age: 3, sleep:'dog bed'}
+    ]
+
+}
